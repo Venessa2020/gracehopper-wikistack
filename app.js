1d4ef6bfd836d63151f4833df678787edfc8b122
Venessa <v_campbell10@yahoo.com>
2020-04-30 12:38:38 -0400

    created models

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const layout = require('./views/layout');
+const { db } = require('./models');
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send(layout(''));
+});
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
 });
 
 const PORT = 3000;
---
+++ public/stylesheets/models/index.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost/5432/wikistack2');
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+const User = db.define('user', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+    
+module.exports = { db };
